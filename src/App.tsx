2647ba4d7478f81c3ab190ddd002d9dec27dcbe8
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-12-09 22:20:37 +0100

    Changed global styles to create global api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 const Container = styled.div`
     max-width: 1200px;
     margin: 0 auto;
 `;
 
+const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    }
+`;
+
 const App: React.FunctionComponent<{}> = ({ children }) => {
     return (
-        <Container>
-            {children}
-        </Container>
+        <>
+            <GlobalStyle />
+            <Container>
+                {children}
+            </Container>
+        </>
     );
 };
 export default hot(App);
