798f182fc6967067463aae59c0ce259ba664df84
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-12-09 22:13:43 +0100

    Added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+`;
 
 const App: React.FunctionComponent<{}> = ({ children }) => {
-    return <>
-        Header
-        {children}
-    </>;
+    return (
+        <Container>
+            {children}
+        </Container>
+    );
 };
 export default hot(App);
