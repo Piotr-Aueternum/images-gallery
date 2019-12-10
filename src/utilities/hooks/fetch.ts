dd61909b0da7f56cb60b8d4c6ab373bc0511768b
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-12-10 23:20:15 +0100

    Extended useFetch by fetching info

--- src/utilities/hooks/fetch.ts
+++ src/utilities/hooks/fetch.ts
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import { Endpoints } from 'models/Endpoints';
 import Axios from 'axios';
+import { FetchStatus } from 'models/FetchStatus';
 
 export const useFetch = <Reply>(endpoint: Endpoints) => {
+    const [status, setStatus] = useState(FetchStatus.Initial);
     const [data, setData] = useState<{ reply: Reply } | { reply: undefined }>({
             reply: undefined,
         });
 
     const fetch = (payload: Object): void => {
+        setStatus(FetchStatus.Fetching);
         Axios.post(`/api/${endpoint}`, { request: payload })
             .then(response => {
+                setStatus(FetchStatus.Fetched);
                 setData({ reply: response.data.reply });
             });
     };
@@ -17,5 +21,6 @@ export const useFetch = <Reply>(endpoint: Endpoints) => {
     return {
         fetch,
         reply: data.reply,
+        status,
     };
 };
