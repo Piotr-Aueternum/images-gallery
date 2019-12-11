9e93129d8ee6bb83dae57f91e0f8e8464d700f35
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-12-11 20:20:19 +0100

    Resetted body margin

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: inherit;
     }
     body {
+        margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     }
 `;
