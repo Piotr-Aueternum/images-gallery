d1ed9de54b0f61d6d5233918988f1cc238fd6f09
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-12-12 00:15:51 +0100

    Added example photo view

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,12 @@ import { Route } from 'react-router';
 import App from './App';
 import { ListCollections } from 'modules/list-collections/ListCollections';
 import { Collections } from 'modules/collections/Collections';
+import { Photo } from 'modules/photo/Photo';
 
 export const Routes = () => (
     <App>
         <Route exact path='/' component={ListCollections}/>
         <Route path='/collection/:id' component={Collections} />
-        <Route path='/photo/:id' component={() => <button>photo</button>} />
+        <Route path='/photo/:id' component={Photo} />
     </App>
 );
