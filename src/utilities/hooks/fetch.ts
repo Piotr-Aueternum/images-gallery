22b20fd063af836edb9059207088a9cd2a5c87d5
Piotr-Aueternum <piotr.aueternum@gmail.com>
2020-01-05 23:31:29 +0100

    Added request cancelation on component unmount

--- src/utilities/hooks/fetch.ts
+++ src/utilities/hooks/fetch.ts
@@ -1,21 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Endpoints } from 'models/Endpoints';
-import Axios from 'axios';
+import Axios, { Canceler } from 'axios';
 import { FetchStatus } from 'models/FetchStatus';
 
 export const useFetch = <Reply>(endpoint: Endpoints) => {
-    const [status, setStatus] = useState(FetchStatus.Initial);
+    const [status, setStatus] =
+        useState(FetchStatus.Initial);
+
     const [data, setData] =
         useState<{ reply?: Reply }>({});
 
+    const [cancelRequest, setCancelRequest] =
+        useState<Canceler | undefined>(undefined);
+
     const fetch = (payload: Object): void => {
         setStatus(FetchStatus.Fetching);
-        Axios.post(`/api/${endpoint}`, { request: payload })
+        Axios.post(`/api/${endpoint}`, {
+            cancelToken: new Axios.CancelToken((cancelFunction) => {
+                cancelFunction();
+                setCancelRequest(cancelFunction);
+            }),
+            request: payload,
+        })
             .then(response => {
                 setStatus(FetchStatus.Fetched);
                 setData({ reply: response.data.reply });
             });
     };
+
+    useEffect(() => {
+        return () => {
+            if (cancelRequest) {
+                cancelRequest();
+            }
+        };
+    });
 
     return {
         fetch,
